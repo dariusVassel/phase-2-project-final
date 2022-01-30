e57c7afcefd70dd074fdc39a069e8aa5a500894b
+ <+>
2022-01-30 15:19:11 -0500

    Product Card Styles

--- phase-2-project-final/src/components/ProductList/ProductCard/ProductCardStyles.js
+++ phase-2-project-final/src/components/ProductList/ProductCard/ProductCardStyles.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-function ProductCardStyles() {
-  return <div></div>;
-}
+export default makeStyles(() => ({
+    root: {
+        // maxWidth: 345, original width style
+        maxWidth: '100%',
+    },
+    media: {
+        height: '50 px',
 
-export default ProductCardStyles;
+        paddingTop: '56.25%', // 16:9
+        marginTop:'30',
+    },
+    cardActions: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    cardContent: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+}));
