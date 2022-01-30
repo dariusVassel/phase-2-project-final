a898f3b2552a2c3a041a7a76f3a52bf5d759e541
+ <+>
2022-01-30 15:08:22 -0500

    NavBar Libraries

--- phase-2-project-final/src/components/NavBar/NavBar.js
+++ phase-2-project-final/src/components/NavBar/NavBar.js
@@ -1,4 +1,11 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Search from "../Search"
+import { AppBar, Toolbar, IconButton, Badge, Typography, Button } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import SortIcon from '@material-ui/icons/Sort'
+import Logo from '../../Assets/logo.jpg'
+import useStyles from './NavBarStyles';
 
 function NavBar() {
   return <div></div>;
