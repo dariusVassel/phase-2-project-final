dd457106fd05f9c3cf27036851827f0c0ebc17e2
+ <+>
2022-01-30 15:16:21 -0500

    Product Form Libraries

--- phase-2-project-final/src/components/ProductList/ProductForm.js
+++ phase-2-project-final/src/components/ProductList/ProductForm.js
@@ -1,4 +1,9 @@
+import { useState } from 'react'
 import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton, Grid, TextField, InputAdornment, Button } from '@material-ui/core';
+import useStyles from './ProductFormStyles';
+import { AddShoppingCart, FavoriteBorder } from "@material-ui/icons";
+import placeholder from "../../Assets/placeholder.jpg"
 
 function ProductForm() {
   return <div></div>;
