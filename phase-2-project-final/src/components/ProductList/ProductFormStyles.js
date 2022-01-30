9c68da8e4a46f57285d0520dd927adb5c6f73db1
+ <+>
2022-01-30 15:17:29 -0500

    Product Form Styles

--- phase-2-project-final/src/components/ProductList/ProductFormStyles.js
+++ phase-2-project-final/src/components/ProductList/ProductFormStyles.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-function ProductFormStyles() {
-  return <div></div>;
-}
-
-export default ProductFormStyles;
+export default makeStyles(() => ({
+    root: {
+        // maxWidth: 345, original width style
+        maxWidth: '100%',
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    cardActions: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    cardContent: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+}));
