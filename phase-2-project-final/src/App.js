4e8662000d466cc2dce1e26b93a533d74e4b0f59
+ <+>
2022-01-30 14:58:31 -0500

    Setup and install react-router-dom

--- phase-2-project-final/src/App.js
+++ phase-2-project-final/src/App.js
@@ -1,5 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import NavBar from './components/NavBar/NavBar';
+import Home from "./components/Home/Home"
+import ProductList from './components/ProductList/ProductList';
+import Cart from '../src/components/Cart/Cart'
+import { Route, Switch, } from 'react-router-dom'
+import ProductForm from './components/ProductList/ProductForm';
+
 
 function App() {
   return (
