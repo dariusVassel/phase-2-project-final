be702accfe4347b67b0fda4139517146beef9a9c
+ <+>
2022-01-30 16:08:50 -0500

    Components reupload

---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import useStyles from './HomeStyles';
+
+import { NavLink } from "react-router-dom";
+import {Button } from '@material-ui/core';
+
+function Home() {
+    const classes = useStyles()
+
+    return (
+        <div className={classes.root}>
+            <div className={classes.roots2} >
+                <Button variant="contained" color ="inherit" size="small" component={NavLink} to="/marketplace">
+                 <strong>Buy & Sell at Aimé Leon Dore</strong>
+                </Button>
+                </div>
+        </div>
+    )
+}
+
+export default Home;
