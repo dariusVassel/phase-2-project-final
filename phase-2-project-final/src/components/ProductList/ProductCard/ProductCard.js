64353f7467361462ea6a64c87f54ce38d2c33eb9
+ <+>
2022-01-30 15:18:07 -0500

    Product Card Libraries

--- phase-2-project-final/src/components/ProductList/ProductCard/ProductCard.js
+++ phase-2-project-final/src/components/ProductList/ProductCard/ProductCard.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart, Favorite, FavoriteBorder } from "@material-ui/icons";
+import useStyles from './productCardStyles';
 
 function ProductCard() {
   return <div></div>;
